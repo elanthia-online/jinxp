78a3551968c0f1c6ab666f5b3d7508b7e3862a44
Benjamin Clos <benjamin.clos@gmail.com>
2021-01-01 08:05:10 -0700

    chore: add proper release mimetype;

--- src/jinx.cr
+++ src/jinx.cr
@@ -3,7 +3,7 @@ require "./manifest"
 require "./build"
 # TODO: Write documentation for `Jinx`
 module Jinx
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 
   def self.build(input : String, output : String)
     Build.new(input:  File.join(Dir.current, input),
